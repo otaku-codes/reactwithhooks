19c7a109c51ecf366ac20fc7a5b61103b4dabf11
otaku-codes <himanshupapola.ph@gmail.com>
2024-04-23 01:10:48 +0530

    first commit

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -73,7 +73,7 @@ export function MovieDetails({
       async function getMovieDetails() {
         setIsLoading(true);
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedID}`
+          `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedID}`
         );
         const data = await res.json();
         setMovie(data);
